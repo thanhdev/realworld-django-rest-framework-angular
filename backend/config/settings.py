e57eb956356bceb8a042969285756d579119ad32
tthanh <ytominhthanh@gmail.com>
2024-04-12 21:08:50 +0700

    BE - profiles - Add follow/unfollow profile api

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+ANGULAR_DIR = BASE_DIR.parent / "frontend/dist/frontend/browser"
 
 
 # Quick-start development settings - unsuitable for production
@@ -63,8 +64,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates', ANGULAR_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,6 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    ANGULAR_DIR
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
