9fc12f8f70c7c0f7a485f62638c62539b70a6825
tthanh <ytominhthanh@gmail.com>
2024-04-13 11:20:14 +0700

    upload static

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates', ANGULAR_DIR],
+        'DIRS': [BASE_DIR / 'templates', BASE_DIR / 'static'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
