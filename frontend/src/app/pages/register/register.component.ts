943936bf4c264be9064d37bc4b8b99f18e96d7ba
Anh Pham <anh.is.a.dev@gmail.com>
2024-04-14 09:30:45 +0700

    Add form validations for register/login

--- frontend/src/app/pages/register/register.component.ts
+++ frontend/src/app/pages/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {RegisterFormViewModel} from "../../common/models/form/authentication-form.view-model";
 
 @Component({
   selector: 'app-register',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  public mainForm: FormGroup<RegisterFormViewModel>;
   public errorDisplayed = false;
+
+  constructor() {
+    this.mainForm = this._constructRegisterForm();
+  }
+
+  private _constructRegisterForm(): FormGroup<RegisterFormViewModel> {
+    return new FormGroup(<RegisterFormViewModel>{
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required]),
+      username: new FormControl('', [Validators.required])
+    });
+  }
 }
