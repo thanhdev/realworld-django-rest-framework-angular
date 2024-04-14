87677296bcc8cbcdb6ce1eb10cb759e6e80f2f0d
tthanh <ytominhthanh@gmail.com>
2024-04-14 13:17:35 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 npm --prefix frontend install
 npm --prefix frontend run build
+rm backend/static/{*.js,*.css}
 python backend/manage.py collectstatic --noinput
 git add backend/static
-git commit -m "Update static files"
+git commit -m "Deploy"
 git push
