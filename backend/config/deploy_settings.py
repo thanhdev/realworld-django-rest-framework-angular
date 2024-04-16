866c43a99e338e840dc06babc8d245ca63c98d78
tthanh <ytominhthanh@gmail.com>
2024-04-16 08:59:26 +0700

    refactor deploy_settings.py

--- backend/config/deploy_settings.py
+++ backend/config/deploy_settings.py
@@ -4,8 +4,5 @@ from config.settings import *
 DEBUG = False
 ALLOWED_HOSTS = ["thanhdev.pythonanywhere.com"]
 CORS_ALLOW_ALL_ORIGINS = True
-STATIC_ROOT = BASE_DIR / 'static/'
-TEMPLATES[0]["DIRS"] = [
-    BASE_DIR / 'templates',
-    BASE_DIR / 'static'
-]
+STATIC_ROOT = BASE_DIR / "static/"
+TEMPLATES[0]["DIRS"] = [BASE_DIR / "templates", BASE_DIR / "static"]
