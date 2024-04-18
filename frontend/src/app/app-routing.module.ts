b6a793f67c19fb0b942d15aba980993a2557ccf7
tthanh <ytominhthanh@gmail.com>
2024-04-18 10:58:01 +0700

    FE - Publish article page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from "./pages/home/home.component";
 import { LoginComponent } from "./pages/login/login.component";
 import { RegisterComponent } from "./pages/register/register.component";
 import { UserSettingsComponent } from "./pages/user-settings/user-settings.component";
+import {EditorComponent} from "./pages/editor/editor.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'settings', component: UserSettingsComponent },
+  { path: 'editor', component: EditorComponent },
   { path: '**', redirectTo: ''}
 ];
 
