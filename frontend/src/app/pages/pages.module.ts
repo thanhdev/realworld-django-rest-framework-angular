e25b0656aa2eb979dbe9bca3adde6db0b53d2bc6
anhptk <anh.is.a.dev@gmail.com>
2024-10-19 12:34:39 +0700

    articles
    Signed-off-by: anhptk <anh.is.a.dev@gmail.com>

--- frontend/src/app/pages/pages.module.ts
+++ frontend/src/app/pages/pages.module.ts
@@ -3,12 +3,8 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { ReactiveFormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
-import { ArticlesFeedComponent } from './article/feed/articles-feed.component';
 import { ArticleComponent } from './article/article.component';
-import { ArticleMetaComponent } from './article/article-meta/article-meta.component';
 import { ProfileComponent } from './profile/profile.component';
-import { ArticleCommentsComponent } from './article/article-comment/article-comments.component';
-import { ArticleTagsComponent } from './article/article-tags/article-tags.component';
 import { MarkdownModule } from "ngx-markdown";
 import { ErrorMessageComponent } from '../shared/error-message/error-message.component';
 import { EditorComponent } from './editor/editor.component';
@@ -17,12 +13,7 @@ import { EditorComponent } from './editor/editor.component';
 @NgModule({
   declarations: [
     HomeComponent,
-    ArticlesFeedComponent,
-    ArticleComponent,
-    ArticleMetaComponent,
     ProfileComponent,
-    ArticleCommentsComponent,
-    ArticleTagsComponent
   ],
   imports: [
     CommonModule,
@@ -30,7 +21,8 @@ import { EditorComponent } from './editor/editor.component';
     RouterModule,
     MarkdownModule.forChild(),
     ErrorMessageComponent,
-    EditorComponent
+    EditorComponent,
+    ArticleComponent
   ],
   exports: [
     HomeComponent
