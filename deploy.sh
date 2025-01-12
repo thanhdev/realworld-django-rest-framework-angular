53ed3c329831b97c5fa973928468ee8abcb2494b
To Thanh <ytominhthanh@gmail.com>
2025-01-12 08:49:05 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ export DJANGO_SETTINGS_MODULE=config.deploy_settings
 npm --prefix frontend install
 npm --prefix frontend run build
 rm backend/static/{*.js,*.css}
+pip install -r backend/requirements.txt
 python backend/manage.py collectstatic --noinput
 git add backend/static
 git commit -m "Deploy"
